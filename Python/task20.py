d1a7c80cd0110dd89e3bba66179b019e0f0e4042
Владимир Рыбин <rvv1983@yandex.ru>
2022-01-30 23:44:54 +0500

    20

--- Python/task20.py
+++ Python/task20.py
@@ -1,2 +1,12 @@
 # Задать номер четверти, показать диапазоны для возможных координат
 quarter = int(input("enter sector"))
+if quarter ==1:
+    print ("x>0,y>0 ")
+if quarter ==2:
+    print ("x<0,y>0 ")
+if quarter ==3: 
+    print ("x<0,y<0 ")
+if quarter ==4:
+    print ("x>0,y<0 ")
+else:
+    print ("please enter number < 5")
