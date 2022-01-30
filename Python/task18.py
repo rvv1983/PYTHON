dce668f4daf6bfa49bb3bb6880286a1f6b230597
Владимир Рыбин <rvv1983@yandex.ru>
2022-01-30 22:43:44 +0500

    18

--- Python/task18.py
+++ Python/task18.py
@@ -1,1 +1,8 @@
 # Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+x=[0,1]
+y=[1,0]
+def check(x,y):
+    for x in range(2):
+        res=not(x or y) == (not x and not y)
+        print(x,y,res)
+check(x,y) 
